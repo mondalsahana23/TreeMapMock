077cc964b008828617c1b1beddf3ec5c3eebfbed
mondalsahana23 <mondalsahana23@gmail.com>
2023-12-19 11:50:49 +0000

    Initial commit

---
+++ TreeMapDemo/src/main/java/MavenDemoProject/TreeMapDemo/TreeMock.java
@@ -0,0 +1,15 @@
+package MavenDemoProject.TreeMapDemo;
+import java.util.*;
+public class TreeMock {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeMap<Integer,String> t =new TreeMap<Integer,String> ();
+		t.put(100, "Rahul");
+		t.put(101, "Karan");
+		t.put(103, "Arjun");
+		System.out.println(t);
+
+	}
+
+}
